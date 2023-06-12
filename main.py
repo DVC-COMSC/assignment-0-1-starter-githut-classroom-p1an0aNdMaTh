8970d0550d536d6e168b867d0203813bd78be542
p1an0aNdMaTh <ltrinh543@insite.4cd.edu>
2023-06-11 21:40:23 -0700

    Create main.py

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 def main():
-
-    ##############################
-    # make your code below
-    # print('Hello World')
-    ##############################
-
+    print('Hello World')
+    print('Python Programming')
 
 if __name__ == '__main__':
     main()
